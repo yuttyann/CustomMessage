93eb395aa019c27df7ebd95b4d5a20cdd23a1adb
yuttyann <misuto546@gmail.com>
2016-06-11 13:35:00 +0900

    v6.1 ランダムMotd、ServerIconを実装

--- src/main/java/com/github/yuttyann/custommessage/listener/ServerListener.java
+++ src/main/java/com/github/yuttyann/custommessage/listener/ServerListener.java
@@ -1,5 +1,7 @@
 package com.github.yuttyann.custommessage.listener;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -12,6 +14,7 @@ import com.github.yuttyann.custommessage.Main;
 import com.github.yuttyann.custommessage.file.Config;
 import com.github.yuttyann.custommessage.packet.ProtocolLibPacket;
 import com.github.yuttyann.custommessage.util.TimeUtils;
+import com.github.yuttyann.custommessage.util.Utils;
 import com.github.yuttyann.custommessage.util.VersionUtils;
 
 public class ServerListener implements Listener {
@@ -28,20 +31,50 @@ public class ServerListener implements Listener {
 			int MaxPlayer = Config.getConfig().getInt("FakeMaxPlayer.MaxPlayer");
 			event.setMaxPlayers(MaxPlayer);
 		}
-		if (Config.getBoolean("Motd.Enable")) {
-			Integer players = event.getNumPlayers();
-			Integer maxplayer = Bukkit.getMaxPlayers();
-			String servername = Bukkit.getServerName();
-			String motd = getMotd();
-			motd = motd.replace("%players", players.toString());
-			motd = motd.replace("%maxplayers", maxplayer.toString());
-			motd = motd.replace("%servername", servername);
-			motd = motd.replace("%version", VersionUtils.getVersion());
-			motd = motd.replace("%time", TimeUtils.getTime());
+		Integer players = event.getNumPlayers();
+		Integer maxplayer = Bukkit.getMaxPlayers();
+		String servername = Bukkit.getServerName();
+		String motd = null;
+		if (Config.getBoolean("RandomMotd.Enable")) {
+			motd = getRandomMotd();
+			motd = replaceMotd(motd, players, maxplayer, servername);
 			motd = motd.replace("&", "§");
 			event.setMotd(motd);
+		} else if (Config.getBoolean("Motd.Enable")) {
+			motd = getMotd();
+			motd = replaceMotd(motd, players, maxplayer, servername);
+			event.setMotd(motd);
+		} else {
+			motd = Utils.getServer().getMotd();
+			motd = replaceMotd(motd, players, maxplayer, servername);
+			event.setMotd(motd);
+		}
+		if (Config.getBoolean("ServerIcon.Enable")) {
+			try {
+				event.setServerIcon(Bukkit.loadServerIcon(getServerIcon()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		setPlayerCountMessage();
+	}
+
+	private String getRandomMotd() {
+		ArrayList<String> arraylist = new ArrayList<String>();
+		for (String set : Utils.getConfigSection("RandomMotd.Message", false)) {
+			arraylist.add(set);
+		}
+		int size = Utils.getRandom().nextInt(arraylist.size());
+		String random = arraylist.get(size);
+		List<String> list = Config.getStringList("RandomMotd.Message." + random);
+		String motd = "";
+		if (list != null && list.size() >= 1 && list.size() <= 2) {
+			motd = list.get(0);
+			if (list.size() == 2) {
+				motd = motd + "\n" + list.get(1);
+			}
+		}
+		return motd;
 	}
 
 	private String getMotd() {
@@ -56,17 +89,28 @@ public class ServerListener implements Listener {
 		return motd;
 	}
 
-	private void setPlayerCountMessage() {
-		if (isProtocolLib() == null) {
-			return;
-		} else if (isProtocolLib()) {
-			new ProtocolLibPacket(plugin).sendPlayerCountMessage();
-		} else {
-			return;
-		}
+	private String replaceMotd(String motd, Integer players, Integer maxplayer, String servername) {
+		motd = motd.replace("%players", players.toString());
+		motd = motd.replace("%maxplayers", maxplayer.toString());
+		motd = motd.replace("%servername", servername);
+		motd = motd.replace("%version", VersionUtils.getVersion());
+		motd = motd.replace("%time", TimeUtils.getTime());
+		motd = motd.replace("&", "§");
+		return motd;
 	}
 
-	private Boolean isProtocolLib() {
-		return plugin.protocollib;
+	private File getServerIcon() {
+		File[] icons = new File(plugin.getDataFolder(), "ServerIcon").listFiles();
+		int size = Utils.getRandom().nextInt(icons.length);
+		if (icons.length > 0) {
+			return icons[size];
+		}
+		return null;
+	}
+
+	private void setPlayerCountMessage() {
+		if (Utils.isPluginEnabled("ProtocolLib")) {
+			new ProtocolLibPacket(plugin).sendPlayerCountMessage();
+		}
 	}
 }
