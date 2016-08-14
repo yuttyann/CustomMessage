bf42e183428ddf82b8459382bcc03fee8722c02b
yuttyann44581 <yuttyann44581>
2016-08-14 12:45:03 +0900

    CustomMessage v6.6

--- src/main/java/com/github/yuttyann/custommessage/TimeManager.java
+++ src/main/java/com/github/yuttyann/custommessage/TimeManager.java
@@ -11,36 +11,38 @@ public class TimeManager {
 		Config.getString("Thursday"), Config.getString("Friday"), Config.getString("Saturday")
 	};
 
+	final private static Calendar calendar = Calendar.getInstance();
+
 	public static Integer getYear() {
-		return Calendar.getInstance().get(Calendar.YEAR);
+		return calendar.get(Calendar.YEAR);
 	}
 
 	public static Integer getMonth() {
-		return Calendar.getInstance().get(Calendar.MONTH) + 1;
+		return calendar.get(Calendar.MONTH) + 1;
 	}
 
 	public static Integer getDay() {
-		return Calendar.getInstance().get(Calendar.DATE);
+		return calendar.get(Calendar.DATE);
 	}
 
-	public static Integer getHour() {
-		return Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+	public static Integer getHour_Of_Day() {
+		return calendar.get(Calendar.HOUR_OF_DAY);
 	}
 
 	public static Integer getMinute() {
-		return Calendar.getInstance().get(Calendar.MINUTE);
+		return calendar.get(Calendar.MINUTE);
 	}
 
 	public static Integer getSecond() {
-		return Calendar.getInstance().get(Calendar.SECOND);
+		return calendar.get(Calendar.SECOND);
 	}
 
-	public static Integer getWeek() {
-		return Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1;
+	public static Integer getDay_Of_Week() {
+		return calendar.get(Calendar.DAY_OF_WEEK) - 1;
 	}
 
 	public static Integer getDay_Of_Year() {
-		return Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+		return calendar.get(Calendar.DAY_OF_YEAR);
 	}
 
 	public static String getTimesofDay() {
@@ -48,10 +50,10 @@ public class TimeManager {
 		timesofday = timesofday.replace("%year", getYear().toString());
 		timesofday = timesofday.replace("%month", getMonth().toString());
 		timesofday = timesofday.replace("%day", getDay().toString());
-		timesofday = timesofday.replace("%hour", getHour().toString());
+		timesofday = timesofday.replace("%hour", getHour_Of_Day().toString());
 		timesofday = timesofday.replace("%minute", getMinute().toString());
 		timesofday = timesofday.replace("%second", getSecond().toString());
-		timesofday = timesofday.replace("%week", week_name[getWeek()]);
+		timesofday = timesofday.replace("%week", week_name[getDay_Of_Week()]);
 		timesofday = timesofday.replace("%dayofyear", getDay_Of_Year().toString());
 		timesofday = timesofday.replace("&", "§");
 		return timesofday;
